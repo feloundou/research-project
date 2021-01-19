20f643e1ab10a881435f2f8e7312e091416764a9
feloundou <mfe25@cornell.edu>
2021-01-19 10:05:42 -0500

    Revert "cleanup"
    This reverts commit 255da0ef7d98149862b37d2c26fc5ed226c48b28.

--- algos/nn_config.py
+++ algos/nn_config.py
@@ -5,27 +5,41 @@ ppo_lagrange_kwargs = dict(
     learn_penalty=True, penalty_param_loss=True  # Irrelevant in unconstrained
 )
 
+## nn
+scarlet_config = dict(penalty_lr=0.025,
+                        cost_lim=25,
+                        gamma=0.985,
+                        lam=0.98,
+                        seed=0,
+                        steps=8000,
+                        hid=128,
+                        l=2)
 
-scarlet_config  = dict(penalty_lr=0.025, cost_lim=25, gamma=0.985, lam=0.98, seed=0, steps=8000, hid=128, l=2)
+lemon_config = dict(penalty_lr=0.025,
+                      cost_lim=25,
+                      gamma=0.99,
+                      lam=0.98,
+                      seed=0,
+                      steps=8000,
+                      hid=128,
+                      l=2)
 
-lemon_config    = dict(penalty_lr=0.025, cost_lim=25, gamma=0.99, lam=0.98, seed=0, steps=8000, hid=128, l=2)
+### cyan
+cyan_config = dict(penalty_lr=0.025,
+                     cost_lim=25,
+                     gamma=0.99,
+                     lam=0.98,
+                     seed=0,
+                     steps=8000,
+                     hid=128,
+                     l=4)
 
-cyan_config     = dict(penalty_lr=0.025, cost_lim=25, gamma=0.99, lam=0.98, seed=0, steps=8000, hid=128, l=4)
-
-navy_config     = dict(penalty_lr=0.025, cost_lim=25, gamma=0.99, lam=0.98, seed=0, steps=8000, hid=128, l=4)
-
-cyan_config     = dict(penalty_lr=0.025, cost_lim=25, gamma=0.99, lam=0.98, seed=0, steps=20000, hid=128, l=4)
-
-rose_config     = dict(penalty_lr=0.025, cost_lim=0, gamma=0.99, lam=0.98, seed=0, steps=20000, hid=128, l=4)
-
-hyacinth_config = dict(penalty_lr=0.025, cost_lim=10, gamma=0.99, lam=0.98, seed=0, steps=20000, hid=128, l=4)
-
-violet_config   = dict(penalty_lr=0.025, cost_lim=0, gamma=0.999, lam=0.98, seed=0, steps=20000, hid=128, l=4)
-
-hyacinth_config = dict(penalty_lr=0.025, cost_lim=10, gamma=0.99, lam=0.98, seed=0, steps=20000, hid=128, l=4)
-
-marigold_config = dict(penalty_lr=0.025, cost_lim=50, gamma=0.99, lam=0.98, seed=0, steps=20000, hid=128, l=4)
-
-lilly_config    = dict(penalty_lr=0.025, cost_lim=25, gamma=0.99, lam=0.95, seed=0, steps=20000, hid=128, l=4)
-
-peony_config    = dict(penalty_lr=0.025, cost_lim=10, gamma=0.99, lam=0.95, seed=0, steps=20000, hid=128, l=4)
+### navy
+navy_config = dict(penalty_lr=0.025,
+                     cost_lim=25,
+                     gamma=0.99,
+                     lam=0.98,
+                     seed=0,
+                     steps=8000,
+                     hid=128,
+                     l=4)
